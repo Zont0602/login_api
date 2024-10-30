6107c85a58347801e898c91ed685206ddae02554
zont <jay0909258118@gmail.com>
2024-10-31 03:24:49 +0800

    add jwt token implementation
    -add create_token , verify_token util functions
    -add verify post api

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from pydantic import EmailStr
 from utils import (
     get_password_hash, 
     verify_password,
-    userinfo_exists
+    userinfo_exists,
+    creat_access_token,
+    verify_token
 )
 from models.user import User
 
@@ -29,7 +31,16 @@ def signup(
                 hashed_password = password_hash,
             )
             session.add(new_user)
-        return {'message' : 'success'}
+        token = creat_access_token({'username' : username, 'email' : email})
+        return {'message' : 'success', 'token' : token}
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail='unkown error')
+    
+@app.post("/verify/{token}")
+def verify(token: str):
+    if verify_token(Session, token):
+        return {'message': 'success'}
+    else:
+        raise HTTPException(status_code=400, detail='verify token failed')
+    
--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 from passlib.context import CryptContext
-from sqlalchemy import select, or_
+from sqlalchemy import select, or_, and_
 from models.user import User
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
 
 pwd_context = CryptContext(schemes=["bcrypt"])
+SECRET_KRY = "my_secret"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_DAYS = 1
 
 def get_password_hash(password):
     return pwd_context.hash(password)
@@ -19,3 +24,37 @@ def userinfo_exists(Session, username, email) -> bool:
         )
         results = session.execute(query).all()
     return len(results) > 0
+
+def creat_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+
+    to_encode["exp"] = expire
+    encoded_jwt = jwt.encode(to_encode , SECRET_KRY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(Session, token):
+    try:
+        payload = jwt.decode(token, SECRET_KRY, algorithms=[ALGORITHM])
+        print(f'payload" {payload}')
+        usermane = payload.get("username")
+        email = payload.get("email")
+        if not usermane or not email:
+            return False
+    except JWTError as e:
+        print(f'error message: {e}')
+        return False
+    
+    with Session.begin() as session:
+        query = (
+            select(User).where(
+                and_(User.username==usermane, User.email==email)
+            )
+        )
+        result_user = session.execute(query).scalar()
+        if not result_user:
+            return False
+        
+        result_user.is_active = True
+        
+    return True
